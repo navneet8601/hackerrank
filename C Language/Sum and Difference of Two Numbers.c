69391fdd3ab671839046a06d330a711f711aea8d
Navneet Singh <42978087+navneet8601@users.noreply.github.com>
2019-09-02 21:43:40 +0530

    Create Sum and Difference of Two Numbers.c

---
+++ C Language/Sum and Difference of Two Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+float roundnum(float);
+int main()
+{
+    int i1,i2;
+    float f1,f2;
+    scanf("%d %d",&i1, &i2);
+    scanf("%f %f",&f1, &f2);
+    printf("%d ",i1+i2);
+    printf("%d",i1-i2);
+    printf("\n%.1f ",f1+f2);
+    printf("%.1f",f1-f2);
+    return 0;
+}
